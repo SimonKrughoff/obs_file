21fd0d51806c43bf335300a0bc97e409ed9c703e
Simon Krughoff <simon.krughoff@gmail.com>
2017-02-17 11:29:53 -0800

    Fix bitrot
    Also add shape_HSM as default

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,5 +1,11 @@
 from lsst.obs.file.fileisr import FileIsrTask
 
+import os.path
+
+from lsst.utils import getPackageDir
+config.calibrate.measurement.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
+config.calibrate.measurement.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
+
 
 config.isr.retarget(FileIsrTask)
 config.calibrate.doAstrometry = False
